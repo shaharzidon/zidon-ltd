0c2ce1b8959b37bf54b34188fe0fcc70e9b3f659
Shahar Zidon <shaharz97@gmail.com>
2024-01-21 13:38:51 +0200

    4 buttons

---
+++ app/routes/menu.tsx
@@ -0,0 +1,15 @@
+export default function Menu() {
+  return (
+    <div>
+      <h1>Menu</h1>
+      <button>Notes</button>
+      <hr></hr>
+      <button>Notes</button>
+      <hr></hr>
+      <button>Notes</button>
+      <hr></hr>
+      <button>Notes</button>
+      <hr></hr>
+    </div>
+  );
+}
